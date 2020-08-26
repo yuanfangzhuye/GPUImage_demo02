3c04f4800c3e66daeb0b49db2cb182ec57ea3e7a
chao.li <chao.li@team-lab.cn>
2020-08-26 12:12:10 +0800

    饱和度滤镜

--- GPUImage_demo02/ViewController.m
+++ GPUImage_demo02/ViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "ViewController.h"
+#import <GPUImage/GPUImage.h>
 
 @interface ViewController ()
+
+//饱和度滤镜
+@property (nonatomic, strong) GPUImageSaturationFilter *saturationFilter;
+
+@property (nonatomic, strong) UIImageView *gpuImageView;
+@property (nonatomic, strong) UIImage *customImage;
 
 @end
 
@@ -16,7 +23,51 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self setupUI];
+}
+
+- (void)setupUI {
+    self.gpuImageView = [[UIImageView alloc] initWithFrame:self.view.bounds];
+    self.gpuImageView.image = [UIImage imageNamed:@"kunkun1.jpeg"];
+    [self.view addSubview:self.gpuImageView];
+    
+    UISlider *slider = [[UISlider alloc] initWithFrame:CGRectMake(0, [UIScreen mainScreen].bounds.size.height - 80, [UIScreen mainScreen].bounds.size.width, 30)];
+    [slider addTarget:self action:@selector(sliderClick:) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:slider];
+    
+    //1.获取图片
+    _customImage = [UIImage imageNamed:@"kunkun1.jpeg"];
+}
+
+- (void)sliderClick:(UISlider *)sender {
+    
+    //2.选择合适的滤镜
+    //饱和度：应用于图像的饱和度或去饱和度（0.0 - 2.0，默认为1.0）
+    if (!_saturationFilter) {
+        _saturationFilter = [[GPUImageSaturationFilter alloc] init];
+        //设置饱和度值
+        _saturationFilter.saturation = 1.0;
+    }
+    
+    //设置要渲染的区域 --图片大小
+    [_saturationFilter forceProcessingAtSize:_customImage.size];
+    //使用单个滤镜
+    [_saturationFilter useNextFrameForImageCapture];
+    //调整饱和度
+    _saturationFilter.saturation = sender.value;
+    
+    //3.创建图片组件--数据源头(静态图片)
+    GPUImagePicture *stillImageSource = [[GPUImagePicture alloc] initWithImage:_customImage];
+    //为图片添加一个滤镜
+    [stillImageSource addTarget:_saturationFilter];
+    //处理图片
+    [stillImageSource processImage];
+    
+    //4.处理完成,从FrameBuffer帧缓存区中获取图
+    UIImage *newImage = [_saturationFilter imageFromCurrentFramebuffer];
+    //更新图片
+    self.gpuImageView.image = newImage;
 }
 
 
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '8.0'
+
+target 'GPUImage_demo02' do
+  
+pod 'GPUImage'
+
+end
